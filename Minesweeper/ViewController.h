6974e9fc6fc49a25efc47dc620ffd63c3ad1abb3
Pre-Sequel <didjerudoo@gmail.com>
2018-05-27 22:43:54 +0500

    Initial Commit

---
+++ Minesweeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Minesweeper
+//
+//  Created by Steve McQueen on 07.02.2018.
+//  Copyright © 2018 Steve McQueen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Minesweeper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Minesweeper
+//
+//  Created by Steve McQueen on 07.02.2018.
+//  Copyright © 2018 Steve McQueen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
